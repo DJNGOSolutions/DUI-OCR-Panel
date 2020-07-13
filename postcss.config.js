781f54f01be7dbd378ed0d670058a3799de385ca
tupic98 <00017316@uca.edu.sv>
2020-07-13 12:21:39 -0600

    Added postcss config

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    // eslint-disable-next-line import/no-extraneous-dependencies,global-require
+    require('autoprefixer'),
+    // eslint-disable-next-line global-require
+    require('tailwindcss'),
+  ],
+};
