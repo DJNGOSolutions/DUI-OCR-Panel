5f5c801b39f2ae06cad65f6689c19da34f70e042
tupic98 <00017316@uca.edu.sv>
2020-07-13 12:21:23 -0600

    Import styles

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
+import '@/assets/css/tailwind.css';
+import '@/assets/css/grid.css';
+import '@/assets/css/normalize.css';
+import '@/assets/css/styles.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
