4d5acb8c9fd92e7189665ba4c0b3e20199a5b3bc
tupic98 <00017316@uca.edu.sv>
2020-07-13 12:21:33 -0600

    Updated Home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="home >w-full">
+    <Stepper />
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Stepper from '@/components/Stepper.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Stepper,
   },
 };
 </script>
