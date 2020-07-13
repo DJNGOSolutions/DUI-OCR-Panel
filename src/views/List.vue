f3f323200dc0d8187e8cb315bdeb84e99fbc694e
tupic98 <00017316@uca.edu.sv>
2020-07-13 12:22:11 -0600

    Added list view

---
+++ src/views/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class=">w-full">
+    <ListTable />
+  </section>
+</template>
+
+<script>
+import ListTable from '@/components/ListTable.vue';
+
+export default {
+  name: 'list',
+  components: {
+    ListTable,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
