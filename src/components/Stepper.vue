fa25d8041cc12b67fc8e815c859cadea154df57b
tupic98 <00017316@uca.edu.sv>
2020-07-17 01:03:52 -0600

    Updated stepper with correct use of form

--- src/components/Stepper.vue
+++ src/components/Stepper.vue
@@ -25,8 +25,15 @@
           <div class="step">
             <div class=">flex >flex-col">
               <p class="loader-title">Carga aquí la imagen de tu DUI</p>
-              <input type="file" class="inputfile" name="file" @change="onFileSelected" ref="fileInput">
-              <label for="file" @click="$refs.fileInput.click()">CHOOSE YOUR FILES</label>
+              <input
+                id="file"
+                type="file"
+                class="inputfile"
+                name="file"
+                ref="fileInput"
+                @change="filesChange($event.target.name, $event.target.files); fileCount = $event.target.files.length"
+              >
+              <label @click="$refs.fileInput.click()">CHOOSE YOUR FILES</label>
             </div>
             <img src="@/assets/img/Step2.png">
           </div>
@@ -34,30 +41,108 @@
         <div :class="step3 ? 'selected':'unselected'">
           <div class="step step3">
             <div>
+              <v-dialog v-model="dialog" persistent max-width="700">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    text
+                    class=">mb-3"
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    <span>Dev Options</span>
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title class="headline"></v-card-title>
+                  <v-card-text>
+                    <div v-if="uploadedFiles && uploadedFiles.length > 0" class=">flex >flex-col">
+                      <div class=">flex >my-2">
+                        <div class=">font-extrabold >text-lg">Eficiencia: </div>
+                        <div class=">text-lg">{{ uploadedFiles[0].confidence.toFixed(2) }}%</div>
+                      </div>
+                      <div class=">flex >flex-col >my-2 >text-lg">
+                        <div class=">font-extrabold">Texto que devuelve el OCR:</div>
+                        <div>{{ uploadedFiles[0].text }}</div>
+                      </div>
+                    </div>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="green darken-1" text @click="dialog = false">Cerrar</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
               <p>El resultado del OCR fue el siguiente, siente en la libertad de hacer los ajustes que creas necesarios.</p>
               <p><b>Nota:</b> Una vez de click en el botón <i>SIGUIENTE</i> estará aceptando guardar estos datos en la base de datos.</p>
               <div class="editing">
-                <input type="text" placeholder="Apellidos">
-                <input type="text" placeholder="Nombres">
-                <input type="text" placeholder="Género">
-                <input type="text" placeholder="Salvadoreño por">
-                <input type="text" placeholder="Fecha de nacimiento">
-                <input type="text" placeholder="Lugar de nacimiento">
-                <input type="text" placeholder="Fecha de expedición">
-                <input type="text" placeholder="Lugar de expedición">
-                <input type="text" placeholder="Fecha de expiración">
-                <input type="text" placeholder="Número de DUI">
-                <input type="text" placeholder="Lugar de residencia">
-                <input type="text" placeholder="Trámite">
-                <input type="text" placeholder="Municipio">
-                <input type="text" placeholder="Departamento">
-                <input type="text" placeholder="Código de zona">
-                <input type="text" placeholder="Nombre de la madre">
-                <input type="text" placeholder="Nombre del padre">
-                <input type="text" placeholder="Nombre del cónyuge">
-                <input type="text" placeholder="Estado familiar">
-                <input type="text" placeholder="Tipo de sangre">
-                <input type="text" placeholder="Profesión">
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Apellidos</span>
+                  <input v-model="form.apellidos" type="text" placeholder="Apellidos">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Nombres</span>
+                  <input v-model="form.nombres" type="text" placeholder="Nombres">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Género</span>
+                  <input v-model="form.genero" type="text" placeholder="Género">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Salvadoreño por</span>
+                  <input v-model="form.salvadorenio_por" type="text" placeholder="Salvadoreño por">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Fecha de nacimiento</span>
+                  <input v-model="form.fecha_nacimiento" type="text" placeholder="Fecha de nacimiento">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Lugar de nacimiento</span>
+                  <input v-model="form.lugar_nacimiento" type="text" placeholder="Lugar de nacimiento">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Fecha de expedición</span>
+                  <input v-model="form.fecha_expedicion" type="text" placeholder="Fecha de expedición">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Lugar de expedición</span>
+                  <input v-model="form.lugar_expedicion" type="text" placeholder="Lugar de expedición">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Fecha de expiración</span>
+                  <input v-model="form.fecha_expiracion" type="text" placeholder="Fecha de expiración">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Número de DUI</span>
+                  <input v-model="form.numero_dui" type="text" placeholder="Número de DUI">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Lugar de residencia</span>
+                  <input v-model="form.residencia" type="text" placeholder="Lugar de residencia">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Trámite</span>
+                  <input v-model="form.tramite" type="text" placeholder="Trámite">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Municipio</span>
+                  <input v-model="form.municipio" type="text" placeholder="Municipio">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Departamento</span>
+                  <input v-model="form.departamento" type="text" placeholder="Departamento">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Código de zona</span>
+                  <input v-model="form.codigo_zona" type="text" placeholder="Código de zona">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Estado familiar</span>
+                  <input v-model="form.estado_familiar" type="text" placeholder="Estado familiar">
+                </label>
+                <label class=">flex >flex-col >px-4">
+                  <span class=">font-bold >ml-2 >my-2">Profesión</span>
+                  <input v-model="form.profesion" type="text" placeholder="Profesión">
+                </label>
               </div>
             </div>
             <img src="@/assets/img/Step3.png">
@@ -74,13 +159,24 @@
       </div>
       <div class="buttons">
         <button @click="clickPrevious" :class="step1 ? 'unactive-button':'active-button'">ANTERIOR</button>
-        <button @click="clickNext" :class="step4 ? 'unactive-button':'active-button'">SIGUIENTE</button>
+        <button @click="clickNext" :class="step4 || nextDisabled ? 'unactive-button':'active-button'">SIGUIENTE</button>
       </div>
     </div>
+    <v-overlay :value="isFileUploading">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
+    <v-overlay :value="savingLoading">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
+const STATUS_INITIAL = 0; const STATUS_SAVING = 1; const STATUS_SUCCESS = 2; const
+  STATUS_FAILED = 3;
+
 export default {
   name: 'Stepper',
   data: () => ({
@@ -88,8 +184,50 @@ export default {
     step2: false,
     step3: false,
     step4: false,
+    dialog: false,
     selectedFile: null,
+    uploadedFiles: [],
+    uploadError: null,
+    uploadFieldName: 'file',
+    isFileUploading: false,
+    savingLoading: false,
+    form: {
+      apellidos: '',
+      nombres: '',
+      genero: '',
+      salvadorenio_por: '',
+      fecha_nacimiento: '',
+      lugar_nacimiento: '',
+      fecha_expiracion: '',
+      lugar_expedicion: '',
+      fecha_expedicion: '',
+      numero_dui: '',
+      residencia: '',
+      tramite: '',
+      municipio: '',
+      departamento: '',
+      codigo_zona: '',
+      estado_familiar: '',
+      profesion: '',
+    },
   }),
+  computed: {
+    nextDisabled() {
+      return this.step2 && this.uploadedFiles && this.uploadedFiles.length === 0;
+    },
+    isInitial() {
+      return this.currentStatus === STATUS_INITIAL;
+    },
+    isSaving() {
+      return this.currentStatus === STATUS_SAVING;
+    },
+    isSuccess() {
+      return this.currentStatus === STATUS_SUCCESS;
+    },
+    isFailed() {
+      return this.currentStatus === STATUS_FAILED;
+    },
+  },
   methods: {
     selectStep(numberStep) {
       this.step1 = false;
@@ -107,7 +245,7 @@ export default {
       }
     },
 
-    clickNext() {
+    async clickNext() {
       if (this.step1 === true) {
         this.step1 = false;
         this.step2 = true;
@@ -115,11 +253,9 @@ export default {
         this.step2 = false;
         this.step3 = true;
       } else if (this.step3 === true) {
-        this.step3 = false;
-        this.step4 = true;
+        await this.saveForm();
       }
     },
-
     clickPrevious() {
       if (this.step4 === true) {
         this.step4 = false;
@@ -132,10 +268,78 @@ export default {
         this.step1 = true;
       }
     },
+    async saveForm() {
+      try {
+        this.savingLoading = true;
+        await axios.post('https://dui-api.herokuapp.com/dui', this.form);
+        this.savingLoading = false;
+        this.step3 = false;
+        this.step4 = true;
+      } catch (e) {
+        console.log(e);
+      } finally {
+        this.savingLoading = false;
+      }
+    },
+    reset() {
+      // reset form to initial state
+      this.currentStatus = STATUS_INITIAL;
+      this.uploadedFiles = [];
+      this.uploadError = null;
+    },
+    save(formData) {
+      // upload data to the server
+      this.currentStatus = STATUS_SAVING;
+      this.isFileUploading = true;
+      this.upload(formData)
+        .then((x) => {
+          this.uploadedFiles = [].concat(x);
+          if (x && Object.entries(x).length > 0) {
+            this.form.fecha_nacimiento = x.data.birthDate;
+            this.form.fecha_expiracion = x.data.expirationData;
+            this.form.nombres = x.data.firstName;
+            this.form.genero = x.data.gender;
+            this.form.fecha_expedicion = x.data.issuanceDate;
+            this.form.lugar_expedicion = x.data.issuancePlace;
+            this.form.apellidos = x.data.lastName;
+            this.form.lugar_nacimiento = x.data.placeOfBirth;
+            this.form.salvadorenio_por = x.data.salvadorian;
+            this.form.numero_dui = x.data.uid;
+          }
+          this.currentStatus = STATUS_SUCCESS;
+          this.isFileUploading = false;
+        })
+        .catch((err) => {
+          this.uploadError = err.response;
+          this.currentStatus = STATUS_FAILED;
+          this.isFileUploading = false;
+        });
+    },
+    filesChange(fieldName, fileList) {
+      // handle file changes
+      const formData = new FormData();
 
-    onFileSelected(event) {
-      const [b] = event.target.files;
-      this.selectedFile = b;
+      if (!fileList.length) return;
+
+      // append the files to FormData
+      Array
+        .from(Array(fileList.length).keys())
+        // eslint-disable-next-line array-callback-return
+        .map((x) => {
+          formData.append(fieldName, fileList[x], fileList[x].name);
+        });
+
+      // save it
+      this.save(formData);
+    },
+    async upload(formData) {
+      // eslint-disable-next-line no-useless-catch
+      try {
+        const { data } = await axios.post('http://localhost:5002/upload', formData);
+        return data;
+      } catch (e) {
+        throw e;
+      }
     },
   },
 };
